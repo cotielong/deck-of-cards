05342dc0e94005e8b3569be8fad4c9b01e6fde37
Cotie Long <longco@us.ibm.com>
2016-02-22 18:00:44 -0500

    running node

---
+++ app.js
@@ -0,0 +1,18 @@
+// set variables for environment
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// views as directory for all template files
+app.set('views', path.join(__dirname, 'views'));     
+// instruct express to server up static assets
+app.use(express.static('public'));
+
+// set routes
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+// Set server port
+app.listen(4000);
+console.log('server is running');
