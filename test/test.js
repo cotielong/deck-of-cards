77ef2ec1a379d641d454471ce10685da380e9766
Cotie Long <longco@us.ibm.com>
2016-02-23 21:42:25 -0500

    tests work

--- test/test.js
+++ test/test.js
@@ -1,26 +1,10 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-      
-/*    it('should test sort', sort() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-      
-    it('should test whether can can be picked', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});*/
-
-var assert = require('assert');
-var converter = require("../public/views/board.js");
+// selection-sort.test.js
+var assert = require('assert', 'document');
+//var selectionSort = require('../public/views/board.js');
 
 describe('Selection Sort Algorithm', function() {
-var cardnames = [];
-var suits = ['clubs', 'spades', 'hearts', 'diams'];
-var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    var numArray = [11, 7, 10, 1, 4];
+    var stringArray = ["a", "ab", "b", "A", "AB", "B"];
 
     it('should sort a number array', function() {
 
